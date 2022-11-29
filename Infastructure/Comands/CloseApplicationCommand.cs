2633b1a20d7373cea1adc06df29e70c871121c7e
RememberTo <fdkirill01@yandex.ru>
2022-11-29 23:55:05 +0300

    start commit

---
+++ Infastructure/Comands/CloseApplicationCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using ChatbotConstructorTelegram.Infastructure.Commands.Base;
+
+namespace ChatbotConstructorTelegram.Infastructure.Commands;
+
+internal class CloseApplicationCommand : Command
+{
+    public override bool CanExecute(object? parameter)
+    {
+        return true;
+    }
+
+    public override void Execute(object? parameter)
+    {
+        Application.Current.Shutdown();
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,80 @@
+﻿using ChatbotConstructorTelegram.Infastructure.Commands;
+using ChatbotConstructorTelegram.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using ChatbotConstructorTelegram.Infastructure;
+
+
+namespace TaskList.ViewModels
+{
+    internal class MainWindowViewModel: ViewModel
+    {
+        private string _title = "Список задач";
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+
+        private string _status = "Готово";
+
+        public string Status
+        {
+            get => _status;
+            set => Set(ref _status, value);
+        }
+
+        #region Command
+
+        #region ResizeMenuCommand
+        public ICommand ResizeMenuCommand { get; }
+
+        private bool OnResizeMenuCommandExecute(object p)
+        {
+            return true;
+        }
+
+        private void CanResizeMenuCommandExecuted(object p)
+        {
+            //if (WidthMenu != 150)
+            //    WidthMenu = 150;
+            //else
+            //    this.WidthMenu = 50;
+        }
+
+        #endregion
+
+        public ICommand CloseApplicationCommand { get; }
+
+        private bool OnCloseApplicationCommandExecute(object p)
+        {
+            return true;
+        }
+
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            #region Command
+
+            ResizeMenuCommand = new LambdaCommand(CanResizeMenuCommandExecuted,OnResizeMenuCommandExecute);
+            CloseApplicationCommand =
+                new LambdaCommand(OnCloseApplicationCommandExecuted, OnCloseApplicationCommandExecute);
+
+            #endregion
+
+        }
+
+    }
+}
